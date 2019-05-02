08b5434786544693d8c3b6fdfa8cad8d5fcba6e7
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-02 20:34:09 +0100

    Prove (A ∧ B) → (B ∧ A)

---
+++ Type_Theory_and_Functional_Programming.lean
@@ -0,0 +1,11 @@
+-- Prove ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+example (p q : Prop) : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
+⟨λ h, ⟨λ hp, h (or.inl hp), λ hq, h (or.inr hq)⟩, 
+  λ hn h, or.elim h hn.1 hn.2⟩
+
+-- Prove (A ∧ B) → (B ∧ A)
+example (A B : Prop) : (A ∧ B) → (B ∧ A) :=
+λ ⟨A, B⟩, ⟨B, A⟩ 
+
+example (A B : Prop) : (A ∧ B) → (B ∧ A) :=
+λ p : (A ∧ B), ⟨p.right, p.left⟩ 
