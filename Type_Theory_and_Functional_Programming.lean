0fd026c0ba1b5ff7d71b0795725a747ba0e265e4
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-03 20:11:42 +0100

    Simplify prove for (A ∧ B) → (B ∧ A) to make it more like in the book

--- Type_Theory_and_Functional_Programming.lean
+++ Type_Theory_and_Functional_Programming.lean
@@ -3,9 +3,11 @@ example (p q : Prop) : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
 ⟨λ h, ⟨λ hp, h (or.inl hp), λ hq, h (or.inr hq)⟩, 
   λ hn h, or.elim h hn.1 hn.2⟩
 
+-- Page 83
 -- Prove (A ∧ B) → (B ∧ A)
 example (A B : Prop) : (A ∧ B) → (B ∧ A) :=
 λ ⟨A, B⟩, ⟨B, A⟩ 
 
+
 example (A B : Prop) : (A ∧ B) → (B ∧ A) :=
-λ p : A ∧ B, ⟨and.right p, and.left p⟩ 
+λ p, ⟨and.right p, and.left p⟩
