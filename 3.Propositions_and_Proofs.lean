51d80c9016cedd67392dc13ec878f8d96384432b
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-07 12:03:54 +0100

    Fix, only the implication to the right was needed

--- 3.Propositions_and_Proofs.lean
+++ 3.Propositions_and_Proofs.lean
@@ -304,13 +304,12 @@ iff.intro
    or.elim hnpq (and.left hnpnq) (and.right hnpnq))
 
 -- Prove ¬p ∨ ¬q → ¬(p ∧ q) := sorry
-example : ¬p ∨ ¬q ↔ ¬(p ∧ q) :=
-iff.intro
+example : ¬p ∨ ¬q → ¬(p ∧ q) :=
 (assume hnpnq : ¬p ∨ ¬q,
  assume hpq : p ∧ q,
   show false, from
   (or.elim hnpnq
     (assume hnp : ¬p, show false, from hnp (and.left hpq))
     (assume hnq : ¬q, show false, from hnq (and.right hpq))
-  ))
-(sorry)
+  )
+)
