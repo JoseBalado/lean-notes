46047746e6239bb6a05ab453e0ca6d52e3547a8b
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-12 19:09:46 +0100

    Initial commit for prove (A ∨ ¬A) → (¬¬A → A)

--- Type_Theory_and_Functional_Programming.lean
+++ Type_Theory_and_Functional_Programming.lean
@@ -36,30 +36,43 @@ example (A B C : Prop) : (A ∧ B) ∧ C → A ∧ (B ∧ C) :=
 example (A B C : Prop) : (A ∧ B) ∧ C → A ∧ (B ∧ C) :=
 λ p, and.intro (and.left (and.left p)) (and.intro (and.right (and.left p)) (and.right p))
  
--- 4.2. Show that the formula (¬A ∨ B) → (A → B) is valid by exhibiting a proof object for it.
+-- 4.2. A) Show that the formula (¬A ∨ B) → (A → B) is valid by exhibiting a proof object for it.
 example (A B : Prop) : (¬A ∨ B) → (A → B) :=
 λ p, or.elim p
 (λ na, λ a, absurd a na)
 (λ b, λ a, b)
 
 example (A B : Prop) : (¬A ∨ B) → (A → B) :=
-(assume hnab : ¬A ∨ B,
+assume hnab : ¬A ∨ B,
 or.elim hnab
 (assume hna : ¬A, 
  assume ha : A,
  show B, from absurd ha hna)
 (assume hb : B, 
  assume ha : A, 
- show B, from hb))
+ show B, from hb)
 
 example (A B : Prop) : (¬A ∨ B) → (A → B) :=
-(assume hnab : ¬A ∨ B,
+assume hnab : ¬A ∨ B,
 or.elim hnab
 (assume hna : ¬A, 
  assume ha : A,
  show B, from false.elim (hna ha))
 (assume hb : B, 
  assume ha : A, 
- show B, from hb))
+ show B, from hb)
 
--- Do you expect the converse, (A → B) → (¬A ∨ B), to be provable?
+-- 4.2 B) Do you expect the converse, (A → B) → (¬A ∨ B), to be provable?
+
+
+-- 4.3. A) Show that from the assumption x : (A ∨ ¬A) that you can derive a
+-- proof object for the formula (¬¬A → A). 
+example (A : Prop) : (A ∨ ¬A) → (¬¬A → A) :=
+assume hana : (A ∨ ¬A),
+or.elim hana
+(assume ha : A,
+assume hna : ¬A, show ¬¬A → A, from sorry)
+(sorry)
+
+-- 4.3 B) Show that you can find a proof
+-- object for the converse, (A → ¬¬A) without this assumption.
