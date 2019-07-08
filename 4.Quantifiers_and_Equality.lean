01517a67a5f8c6ab28c149e41463cdac4de2630c
Jose Balado <JoseBalado@users.noreply.github.com>
2019-07-08 12:59:03 +0100

    Prove (∀ x : α, p x ∧ q x) → ∀ y : α, p y

---
+++ 4.Quantifiers_and_Equality.lean
@@ -0,0 +1,12 @@
+-- Prove (∀ x : α, p x ∧ q x) → ∀ y : α, p y
+variables (α : Type) (p q : α → Prop)
+
+example : (∀ x : α, p x ∧ q x) → ∀ y : α, p y  :=
+assume h : ∀ x : α, p x ∧ q x,
+assume y : α,
+show p y, from (h y).left
+
+example : (∀ x : α, p x ∧ q x) → ∀ y : α, p y  :=
+assume h : ∀ x : α, p x ∧ q x,
+assume y : α,
+show p y, from and.left (h y)
