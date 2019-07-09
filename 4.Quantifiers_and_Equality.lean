abb8007d0365f0d7354aa3c4671a51cc3e7c83f7
Jose Balado <JoseBalado@users.noreply.github.com>
2019-07-09 10:50:02 +0100

    using the same variable for the conclusion

--- 4.Quantifiers_and_Equality.lean
+++ 4.Quantifiers_and_Equality.lean
@@ -10,3 +10,11 @@ example : (∀ x : α, p x ∧ q x) → ∀ y : α, p y  :=
 assume h : ∀ x : α, p x ∧ q x,
 assume y : α,
 show p y, from and.left (h y)
+
+-- Remember that expressions which differ up to renaming of bound variables are considered to
+-- be equivalent. So, for example, we could have used the same variable, x, in both the hypothesis
+-- and conclusion, and instantiated it by a different variable, z, in the proof:
+example : (∀ x : α, p x ∧ q x) → ∀ x : α, p x  :=
+assume h : ∀ x : α, p x ∧ q x,
+assume z : α,
+show p z, from and.left (h z)
