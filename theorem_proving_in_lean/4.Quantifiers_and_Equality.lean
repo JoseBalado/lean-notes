1dd7e0bc740a33bfeebd8a8f75fda9b8302fd21a
Jose Balado <JoseBalado@users.noreply.github.com>
2019-07-16 15:10:22 +0100

    Prove (h : ∃ x, p x ∧ q x) : ∃ x, q x ∧ p x

--- theorem_proving_in_lean/4.Quantifiers_and_Equality.lean
+++ theorem_proving_in_lean/4.Quantifiers_and_Equality.lean
@@ -81,3 +81,11 @@ example (x y z : ℕ) (hxy : x < y) (hyz : y < z) :
 exists.intro y (and.intro hxy hyz)
 
 #check @exists.intro
+
+variables (α : Type) (p q : α → Prop)
+
+example (h : ∃ x, p x ∧ q x) : ∃ x, q x ∧ p x :=
+exists.elim h
+  (assume w,
+    assume hw : p w ∧ q w,
+    show ∃ x, q x ∧ p x, from ⟨w, hw.right, hw.left⟩)
