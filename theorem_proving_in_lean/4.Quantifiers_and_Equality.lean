4a439bf10ac1f187c8b4a4d93ed8bfe40cee368f
Jose Balado <JoseBalado@users.noreply.github.com>
2019-07-16 14:44:59 +0100

    example (x y z : ℕ) (hxy : x < y) (hyz : y < z)

--- theorem_proving_in_lean/4.Quantifiers_and_Equality.lean
+++ theorem_proving_in_lean/4.Quantifiers_and_Equality.lean
@@ -74,3 +74,9 @@ exists.intro 4 h
 
 example (x : ℕ) (h : x > 0) : ∃ y, y < x :=
 exists.intro 0 h
+
+example (x y z : ℕ) (hxy : x < y) (hyz : y < z) :
+  ∃ w, x < w ∧ w < z :=
+exists.intro y (and.intro hxy hyz)
+
+#check @exists.intro
