2fa0c90d1ec24fbb51964a2e54dffb1ef2302c7e
Jose Balado <JoseBalado@users.noreply.github.com>
2019-07-17 10:31:18 +0100

    Making more clear the meaning of ⟨w, hw.right, hw.left⟩

--- theorem_proving_in_lean/4.Quantifiers_and_Equality.lean
+++ theorem_proving_in_lean/4.Quantifiers_and_Equality.lean
@@ -88,4 +88,4 @@ example (h : ∃ x, p x ∧ q x) : ∃ x, q x ∧ p x :=
 exists.elim h
   (assume w,
     assume hw : p w ∧ q w,
-    show ∃ x, q x ∧ p x, from ⟨w, hw.right, hw.left⟩)
+    show ∃ x, q x ∧ p x, from ⟨w, ⟨hw.right, hw.left⟩⟩)
